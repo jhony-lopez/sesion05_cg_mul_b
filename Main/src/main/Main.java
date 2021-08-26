519a6ce5fbd30b024befe68fbac03045c0bb5271
jhony-lopez <jhonyanlopeztole7@gmail.com>
2021-08-26 15:59:18 -0500

    Signed-off-by: jhony-lopez <jhonyanlopeztole7@gmail.com>

--- Main/src/main/Main.java
+++ Main/src/main/Main.java
@@ -1,5 +1,7 @@
 package main;
 
+import java.util.Scanner;
+
 public class Main
 {
 	public static void main(String[] args) {
@@ -7,23 +9,62 @@ public class Main
 		// Solicite al usuario ingresar el valor de un vector de traslación 
 		// Aplique el vector de traslación anterior a cada uno de los objetos referidos
 		// Muestre las posiciones originales y finales de cada uno de los objetos referidos
-		
+                Cuadrado cuad = new Cuadrado();
+                Circunferencia cir = new Circunferencia();
+                Scanner l = new Scanner(System.in);
+                System.out.println("Ingrese coordenada inicial");
+                System.out.println("Ingrese x");
+                int xi = l.nextInt();
+                System.out.println("Ingrese y");
+                int yi = l.nextInt();
+                System.out.println("Ingrese coordenada final");
+                System.out.println("Ingrese x");
+                int xf = l.nextInt();
+                System.out.println("Ingrese y");
+                int yf = l.nextInt(); 
+                System.out.println("Ingrese el lado del cuadrado");
+                int lado = l.nextInt();
 	}
 }
-
+ 
 class Coord {
     private int x, y;
+ 
+    public Coord(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+    
 }
 
 class Cuadrado {
     private Coord c;
     private int lado;
+    public Cuadrado(){
+    }
     public Cuadrado(Coord c, int lado) {
         this.c = c;
         this.lado = lado;
     }
-    public void Traslacion() {  // Complete los parámetros requeridos
-        
+    public void Traslacion(Coord t) {  // Complete los parámetros requeridos
+        c.setX(t.getX()+c.getX());
+        c.setY(t.getY()+c.getY());      
     }
     public void Escalado() {  // Complete los parámetros requeridos
         
@@ -37,6 +78,23 @@ class Circunferencia {
     Coord c;
     float radio;
     // Implementar los métodos Traslacion, Escalado y Rotacion para ésta clase
+    
+    public Circunferencia(){
+     }
+    public Circunferencia(Coord c, float radio) {
+        this.c = c;
+        this.radio = radio;
+    }
+    
+    public void Traslacion() { 
+        
+    }
+    public void Escalado() {  
+        
+    }
+    public void Rotacion() { 
+        
+    }
 }
 
 
